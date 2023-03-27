8b2b4f88325608771adf3bf8444821b513e36056
Hollis Vohr <hollis.vohr@gmail.com>
2023-03-27 16:44:51 -0600

    Add finished dragon functions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,40 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
 
+  }
+  return dragon;
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+ function eat(dragon) {
+  dragon.timesEaten += 1
+  if (dragon.timesEaten  > 2) {
+    dragon.hungry = false;
+  }
+  return dragon;
+ }
+
+ function findFireBreathers(dragon) {
+  var fireBreathers = []
+  for (var i = 0; i < dragon.length; i++) {
+    if (dragon[i].temperment === "aggressive" ) {
+      fireBreathers.push(dragon[i])
+    }
+  }
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
