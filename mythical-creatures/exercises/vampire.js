85f73b952cddc59c433f9b1d19ba8d252ed65f0a
Hollis Vohr <hollis.vohr@gmail.com>
2023-03-27 18:15:18 -0600

    Add finished vampire functions

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,53 @@
+function createVampire(name, pet, thirsty, ouncesDrank) {
+  var vampire = {
+    name: name,
+    pet: pet || "bat",
+    thirsty: true,
+    ouncesDrank: 0
+  }
+  return vampire;
+}
 
+function encounterDeliciousVictim(vampire) {
+  if (vampire.thirsty === true) {
+    return "I WANT TO SUCK YOUR BLOOD!";
+  } else
+  return "No thanks, I am too full.";
+}
+
+function drink(vampire) {
+  vampire.ouncesDrank += 10;
+  if (vampire.ouncesDrank >= 50) {
+    vampire.thirsty = false;
+    vampire.ouncesDrank = 50;
+  } 
+  return vampire;
+}
+
+function inquirePlace(locations, location) {
+  var response = `No, I have never been to ${location}.`
+  for (var i = 0; i < locations.length; i++) {
+    if (locations[i] === location) {
+    return `Yes, I have spent some time in ${location}.`
+    }
+  }
+return response;
+}
+
+function findBatLovers(vampire) {
+  var batLover = []
+  for (var i = 0; i < vampire.length; i++) {
+    if (vampire[i].pet === "bat") {
+      batLover.push(vampire[i].name)
+    }
+  }
+  return batLover;
+}
 
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+  createVampire, 
+  drink, 
+  findBatLovers, 
+  encounterDeliciousVictim, 
+  inquirePlace
 }
