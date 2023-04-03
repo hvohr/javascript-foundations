92c19202dfa8bb3acf788e1643ca97f92de679aa
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-03 16:24:54 -0600

    Finish answers for favoriteFood functions

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,34 @@
+function createFavoriteFood(food) {
+    var dish = {
+        name: food.dish,
+        ingredients: food.ingredients,
+        isSpicy: food.isSpicy,
+        timesOrdered: 0
+    }
+    return dish;
+}
 
+function commentOnSpiciness(food) {
+    if (food.isSpicy === true) {
+        return `Wow, this ${food.name} is so spicy!`
+    } else {
+        return `Phew, this ${food.name} is not very spicy.`
+    }
+}
 
+function orderFood(dish) {
+   dish.timesOrdered++
+   return dish;
+ }
 
-module.exports = { };
+ function createShoppingList(list) {
+    var shoppingList = [].concat(list[0].ingredients, list[1].ingredients)
+    return shoppingList;
+ }
+
+module.exports = {
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+};
