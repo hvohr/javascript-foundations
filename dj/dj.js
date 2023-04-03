38e5648b09e4f4540f8ce4bf7530196e02216050
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-02 22:19:46 -0600

    Add finished dj functions

--- dj/dj.js
+++ dj/dj.js
@@ -1,4 +1,42 @@
 
+function createSong(name, artist) {
+    var song = {
+        name: name,
+        artist: artist,
+        hasBeenPlayed: false
+    }
+    return song;
+}
+
+function playSong(song) {
+    song.hasBeenPlayed = true;
+    return song;
+}
+
+function makePlaylist(name, songs) {
+    var playlist = {
+        name: name,
+        songs: songs
+    }
+    return playlist;
+}
+
+function addSongToPlaylist(playlist, song) {
+    playlist.songs.push(song)
+    return playlist;
+}
+
+function playAllSongs(playlist) {
+    for (var i = 0; i < playlist.songs.length; i++)
+    playlist.songs[i].hasBeenPlayed = true;
+    return playlist;
+    }
 
 
-module.exports = { };
+module.exports = {
+    createSong,
+    playSong,
+    makePlaylist,
+    addSongToPlaylist,
+    playAllSongs
+};
