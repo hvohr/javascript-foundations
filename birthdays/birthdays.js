44d1d212c8d36b3fa504923bfe5a5b11d8bce309
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-04 10:17:34 -0600

    Add finished birthday functions

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,29 @@
+function createBirthday(name, month, day) {
+    var birthday = {
+        name,
+        month,
+        day,
+    }
+    return birthday;
+}
 
 
-module.exports = {  };
+function celebrateBirthday(birthday) {
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
+
+function countBirthdays(birthdays, month) {
+    var count = 0
+    for (var i = 0; i < birthdays.length; i++) {
+        if (birthdays[i].month === month) {
+            count++
+        }
+    }
+    return count
+   }
+
+module.exports = {
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+};
