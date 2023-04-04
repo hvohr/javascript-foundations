af9c6e8e12c60800006d0a7ee09ef19dcbfa0140
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-03 22:39:54 -0600

    Add finished video game functions

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,44 @@
+function createPlayer(name, age, moveset) {
+    var player = {
+        name: name,
+        age: age,
+        moveset: moveset
+
+    }
+    return player;
+}
+
+function createLevel(name, players) {
+    var level = {
+        name: name,
+        players: players,
+        coins: 0,
+        lives: 3
+    }
+    return level
+}
+
+function findCoins(level, newCoins) {
+    level.coins += newCoins;
+    if (newCoins === 100) {
+        level.lives += 1;
+    }
+    return level;
+}
+
+function defeatPlayer(level) {
+    level.lives--
+    if (level.lives === 0) {
+     return 'GAME OVER';
+    }
+    return level;
+}
 
 
 
-module.exports = { };
+module.exports = {
+    createPlayer,
+    createLevel,
+    findCoins,
+    defeatPlayer
+};
