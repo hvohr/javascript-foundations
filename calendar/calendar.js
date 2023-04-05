e7b758250fe52ad19e7b4b0d31f9c6f7a632a30b
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-05 12:35:49 -0600

    Add finished calendar functions

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,35 @@
+function createEvent(title, month, day) {
+    var event = {
+        title,
+        month,
+        day
+    }
+    if (day >= 32 || day <= 0) {
+        return `Error: ${day} is not a valid day`
+    }
+    return event;
+}
 
+function createCalendar(owner, events) {
+    var calendar = {
+        owner,
+        events
+    }
+    return calendar;
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendar, month) {
+    var monthlyEvents = []
+    for (var i = 0; i < calendar.events.length; i++) {
+        if (calendar.events[i].month === month) {
+            monthlyEvents.push(calendar.events[i])
+        }
+    }
+    return monthlyEvents;
+}
+
+module.exports = {
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+};
