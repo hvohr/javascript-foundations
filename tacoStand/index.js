b1448c27d39f6105be6ea72f19abe69365d857b4
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-05 22:14:51 -0600

    Add answers for tacoStand functions

--- tacoStand/index.js
+++ tacoStand/index.js
@@ -1,2 +1,33 @@
+function createIngredient(name = 'unknown', price = 0.00) {
+    var ingredient = {
+        name: name,
+        price: price
+    }
+    return ingredient;
+}
 
-module.exports = {}
+
+function createTaco(name = 'custom', ingredients = []) {
+    return { name, ingredients };
+}
+
+function addIngredientToTaco(taco, ingredient = {}) {
+    taco.ingredients.push(ingredient)
+    return taco;
+}
+
+
+function calculatePrice(taco) {
+    var sum = 0;
+    for (var i = 0; i < taco.ingredients.length; i++) {
+        sum += taco.ingredients[i].price
+    }
+    return sum;
+}
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
