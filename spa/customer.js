6867ed470db2ff6b6ae2824d159119ca296fda17
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-11 09:07:08 -0600

    Finish spa functions and test material

--- spa/customer.js
+++ spa/customer.js
@@ -1,2 +1,52 @@
+function createCustomer(name, bill, bookings = []) {
+    var customer = {
+        name: name,
+        bill: bill,
+        bookings: bookings
+    }
+    return customer;
+}
 
-module.exports = {  }
+
+function greeting(customer) {
+    if (customer.bookings.length >= 1) {
+        return `${customer.name}! Welcome back to Happy Spa`
+    } else {
+        return `${customer.name}! Welcome to Happy Spa`
+    }
+}
+
+function createService(name, cost) {
+    if (name === undefined || cost === undefined) {
+        return "Please provide service name and cost."
+    } else {
+        return { name: name, cost: cost }
+    }
+}
+
+function bookServices(customer, service) {
+    customer.bookings.push(service.name)
+    customer.bill += service.cost
+    return customer
+}
+
+
+
+function applyGiftCard(allServices, giftCard) {
+    var affordable =[];
+        for (var i = 0; i < allServices.length; i++) {
+        if (allServices[i].price <= giftCard) {
+            affordable.push(allServices[i].name)
+        }
+    }
+    return affordable
+}
+
+
+module.exports = {
+    createCustomer,
+    greeting,
+    bookServices,
+    createService,
+    applyGiftCard
+}
