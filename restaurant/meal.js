40ec5ebbf605b9a4266f13ff54f774b532f68ea9
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-12 15:07:15 -0600

    Add finished restaurant functions

--- restaurant/meal.js
+++ restaurant/meal.js
@@ -1,2 +1,57 @@
+function createMeal(name, specialRequests, tableNumber) {
+    var meal = {
+        name: name,
+        specialRequests: specialRequests,
+        tableNumber: tableNumber,
+        complete: false
+    }
+    return meal
+}
 
-module.exports = { };
+function getMade(meal) {
+    meal.complete = true
+    return meal
+}
+
+function announceMeal(meal) {
+    if (meal.complete === true) {
+        return `Order up - ${meal.name} for table ${meal.tableNumber}!`
+    } else {
+        return `This ${meal.name} is not completed yet`
+    }
+}
+
+
+function createOrder({ tableNumber: tableNumber, meals: meals, completedMeals = [] }) {
+    var order = {
+        tableNumber: tableNumber,
+        meals: meals,
+        completedMeals: completedMeals
+    }
+    return order
+}
+
+
+function cookMeal(order, meal) {
+if (order.tableNumber === meal.tableNumber) {
+    order.completedMeals.push(meal.name)
+}
+return order
+}
+
+function listOrders(order) {
+var mealNames = []
+for (var i = 0; i < order.meals.length; i++) {
+    mealNames.push(order.meals[i].name)
+}
+return mealNames;
+}
+
+module.exports = {
+    createMeal,
+    getMade, 
+    createOrder,
+    cookMeal, 
+    listOrders, 
+    announceMeal
+};
