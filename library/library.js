b783b7dcf93e296704f983962d7af98b3696a049
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-14 09:13:35 -0600

    Add finished library functions and test

--- library/library.js
+++ library/library.js
@@ -1,3 +1,31 @@
 
+function issueCard(name, age, numBooksCheckedOut) {
+  var libraryCard = {
+    name: name,
+    age: age,
+    numBooksCheckedOut: 0,
+    isChild: false
+  }
+  if (libraryCard.age < 12) {
+    libraryCard.isChild = true
+  }
+  return libraryCard;
+}
 
-module.exports = { };
+function searchByAuthor(collection, author) {
+  var array = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i].author === author) {
+      array.push(collection[i])
+    }
+  }
+  if (array.length === 0) {
+    return 'No book found for search criteria'
+  }
+  return array
+}
+
+module.exports = {
+  issueCard,
+  searchByAuthor
+};
