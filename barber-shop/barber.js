6ca0307e9ff1621bfec2da5af92f6187ef5d29be
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-13 20:19:44 -0600

    Add finished barber functions and test

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,36 @@
+function createBarber(name, earnings = 0, haircuts = []) {
+    var barber = {
+        name: name,
+        earnings: earnings,
+        haircuts: haircuts
+    }
+    return barber;
+}
+
+function giveCompliment(haircut) {
+    return `This ${haircut.style} looks great!`
+}
+
+function cutHair(barber, haircut ) {
+    barber.haircuts.push(haircut)
+    barber.earnings += haircut.price
+    return barber
+}
 
 
-module.exports = {  };
+function listStyles(barber, length) {
+var styles = []
+for (var i = 0; i < barber.haircuts.length; i++) {
+    if (barber.haircuts[i].hairLength === length) {
+        styles.push(barber.haircuts[i].style)
+    }
+}
+return styles
+}
+
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+};
