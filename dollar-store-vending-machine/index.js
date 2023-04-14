9a9df03b802bc06d78155cfa14c86df56b727da9
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-14 13:50:53 -0600

    Add finished functions for dollar store vending machine and test

--- dollar-store-vending-machine/index.js
+++ dollar-store-vending-machine/index.js
@@ -1,2 +1,35 @@
+function createItemStock(name = 'unknown', quantity = 0, price = 1.00) {
+    var item = {
+        name: name,
+        quantity: quantity,
+        price: price
+    }
+    return item
+}
 
-module.exports = {}
+function makePurchase(item, money) {
+    if (item.price > money) {
+        return `Sorry, you need at least $${item.price} to make that purchase`
+    }
+    if (item.quantity === 0) {
+        return `Sorry, there are none left`
+    } else {
+        return `Here are your ${item.name}`
+    }
+}
+
+
+function collectChange(change) {
+var sum = 0
+    for (var i = 0; i < change.length; i++) {
+        sum += change[i]
+    }
+    return sum
+}
+
+
+module.exports = {
+    createItemStock,
+    collectChange,
+    makePurchase
+}
