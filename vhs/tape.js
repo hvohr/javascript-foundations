cea6e0f0a36b5ac5959ccc8e97edb9fbf1efa6a6
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-14 09:54:46 -0600

    Add finished vhs functions

--- vhs/tape.js
+++ vhs/tape.js
@@ -1,3 +1,34 @@
+function createTape(title, readyToPlay = false) {
+    var tape = {
+        title: title,
+        readyToPlay: readyToPlay
+    }
+    return tape
+}
 
 
-module.exports = {  }
+function reset(tape) {
+   if (tape.readyToPlay === false) {
+     tape.readyToPlay = true
+   }
+   return tape
+}
+
+
+function createCollection(tape) {
+collection = []
+ for (var i = 0; i < tape.length; i++) {
+    collection.push(tape[i].title)
+ }
+ if (collection.length === 0) {
+    return "Your collection is empty"
+ }
+ return collection
+}
+
+
+module.exports = {
+    createTape,
+    reset,
+    createCollection
+}
