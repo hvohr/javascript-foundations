d62f8dc07812195031b9bbbf96f0fcd779f10fe2
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-17 13:55:41 -0600

    Add finished meal planning functions

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,32 @@
+function createMeal(type, calorieGoal, dishes = []) {
+    var meal = {
+        type: type,
+        calorieGoal: calorieGoal,
+        dishes: dishes
+    }
+    return meal;
+}
 
-module.exports = { }
+function addDish(meal, dish) {
+    if (dish.calories <= meal.calorieGoal) {
+        meal.dishes.push(dish)
+        meal.calorieGoal -= dish.calories
+    }
+
+    return meal
+}
+
+
+function calculateCalories(meal) {
+    var sum = 0
+    for (var i = 0; i < meal.dishes.length; i++) {
+        sum += meal.dishes[i].calories
+    }
+    return `${meal.type} has a total of ${sum} calories.`
+}
+
+module.exports = {
+    createMeal,
+    addDish,
+    calculateCalories
+}
